f10409776cc949f33f8d98cf089afba5e771ce21
Ichiro Dohi <ichi-dohi@i-mobile.co.jp>
2017-06-01 11:05:43 +0900

    Fixed build tool to build using build.sh’s file path

--- build.sh
+++ build.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
-BUILD="./build"
+BIN="$(cd $(dirname $0) && /bin/pwd)"
+
+TMP_UNZIP="${BIN}/unzipped"
+BUILD="${BIN}/build"
 BUILD_ANDROID="${BUILD}/android"
 BUILD_IOS="${BUILD}/ios"
 BUILD_DEFAULT="${BUILD}/default"
-MAIO_EXTENSION="./MaioExtension"
-MAIO_EXTENSION_DEFAULT="MaioExtensionDefault"
-ANDROID_JAR="./MaioAdobeAirExtensionBase/adobeairextension/build/intermediates/bundles/debug/classes.jar"
-IOS_LIB="./IOSMaioExtension/IOSMaioExtension/build/release/libIOSMaioExtension.a"
+MAIO_EXTENSION="${BIN}/MaioExtension"
+MAIO_EXTENSION_DEFAULT="${BIN}/MaioExtensionDefault"
+ANDROID_JAR="${BIN}/MaioAdobeAirExtensionBase/adobeairextension/build/intermediates/bundles/debug/classes.jar"
+IOS_LIB="${BIN}/IOSMaioExtension/IOSMaioExtension/build/release/libIOSMaioExtension.a"
 
 # actual extension
 echo "> Copying extension base"
-unzip $MAIO_EXTENSION/bin/MaioExtension.swc -d ./unzipped
-sudo cp -f ./unzipped/library.swf $BUILD_ANDROID
-sudo cp -f ./unzipped/library.swf $BUILD_IOS
-rm -rf ./unzipped
-sudo cp -f $MAIO_EXTENSION/bin/MaioExtension.swc $BUILD
+unzip $MAIO_EXTENSION/bin/MaioExtension.swc -d $TMP_UNZIP
+cp -f $TMP_UNZIP/library.swf $BUILD_ANDROID
+cp -f $TMP_UNZIP/library.swf $BUILD_IOS
+rm -rf $TMP_UNZIP
+cp -f $MAIO_EXTENSION/bin/MaioExtension.swc $BUILD
 
 # default extension
 echo "> Copying default extension"
-unzip $MAIO_EXTENSION_DEFAULT/bin/MaioExtensionDefault.swc -d ./unzipped
-sudo cp -f ./unzipped/library.swf $BUILD_DEFAULT
-rm -rf ./unzipped
+unzip $MAIO_EXTENSION_DEFAULT/bin/MaioExtensionDefault.swc -d $TMP_UNZIP
+cp -f $TMP_UNZIP/library.swf $BUILD_DEFAULT
+rm -rf $TMP_UNZIP
 
 # android extension
 if [ ! -e $ANDROID_JAR ]; then
@@ -39,6 +42,7 @@ else
     mv -f $IOS_LIB $BUILD_IOS/libIOSMaioExtension.a
 fi
 
+# generate ANE
 cd $BUILD
 echo "> Building"
 adt -package -target ane MaioExtension.ane extension.xml -swc MaioExtension.swc \
